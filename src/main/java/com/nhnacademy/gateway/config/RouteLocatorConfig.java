9f2984bdbbf2ce5b47ecd3be1dca311ddaa3a18c
BHwi <bhwi0422@gmail.com>
2024-03-19 16:49:18 +0900

    mod: error

--- src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
+++ src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
@@ -4,13 +4,6 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @Configuration
 public class RouteLocatorConfig {
