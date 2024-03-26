e5ec395ca7d7ad1057ff99499322181b66298e79
jinjiwon <jinjw5441@gmail.com>
2024-03-26 13:43:49 +0900

    fix: redis session

--- src/main/java/com/nhnacademy/gateway/controller/RedisController.java
+++ src/main/java/com/nhnacademy/gateway/controller/RedisController.java
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @RequestMapping("/redis")
 @RestController
@@ -19,6 +17,13 @@ public class RedisController {
     public ResponseEntity<Object> getRedisKey(@PathVariable String key) {
         ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
         String value = valueOperations.get(key);
+        return ResponseEntity.status(HttpStatus.OK).body(value);
+    }
+
+    @PostMapping("/{key}")
+    public ResponseEntity<Object> setRedisKey(@PathVariable String key, @RequestBody String value) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, value);
         return ResponseEntity.status(HttpStatus.OK).body(value);
     }
 }
